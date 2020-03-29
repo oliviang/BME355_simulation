3169ef413ab8a72c388d38835f9aad46d51ddd77
andrea-haw <alhaw@uwaterloo.ca>
2020-03-29 14:48:02 -0400

    improved plots except for toe clearance

--- FESsignal.py
+++ FESsignal.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def trapezoid_wave(t, width=2., slope=1., amp=1.):
-    a = slope*width*signal.sawtooth(2*np.pi*t/width, width=0.5)/4.
+def trapezoid_wave(t, width=2., slope=1, amp=1.):
+    a = slope*width*signal.sawtooth(2*np.pi*(t+0.12)/width, width=0.5)/4.
     a[a>amp/2.] = amp/2.
     a[a<-amp/2.] = -amp/2.
     return a + amp/2.
 
-t = np.linspace(0, 6, 501)
+t = np.linspace(0, 0.40625, 501)
 l = "width=2, slope=2, amp=1"
-plt.plot(t,trapezoid_wave(t, width=3, slope=2, amp=0.5), label=l)
+plt.plot(t,trapezoid_wave(t,width=0.65,slope=7,amp=1), label=l)
 
 plt.legend()
 plt.show()
